3300db2cccd1b3a2956a9c991f35d6fbd26226c3
MalakMustafa7 <malakmustafa725@gmail.com>
2025-08-12 00:42:24 +0300

    GreenStageCommit

--- Accumulator/src/main/java/stopwatch/StopWatch.java
+++ Accumulator/src/main/java/stopwatch/StopWatch.java
@@ -1,3 +1,41 @@
 package stopwatch;
 public class StopWatch {
+    private int minutes=0;
+    private int hours = 0;
+    private int days = 0;
+    private int dailyWorkingHours=24 ;
+    public StopWatch(){
+
+    }
+    public StopWatch(int dailyWorkingHours){
+        this.dailyWorkingHours=dailyWorkingHours;
+    }
+
+    public void record(int minutes) {
+        if (minutes < 0) return;
+
+        this.minutes += minutes;
+
+        if (this.minutes >= 60) {
+            this.hours += this.minutes / 60;
+            this.minutes = this.minutes % 60;
+        }
+
+        if (this.hours >= dailyWorkingHours) {
+            this.days += this.hours / dailyWorkingHours;
+            this.hours = this.hours % dailyWorkingHours;
+        }
+
+    }
+
+    public int getMinutes(){
+        return this.minutes;
+    }
+
+    public int getHours() {
+        return this.hours;
+    }
+    public  int getDays(){
+        return this.days;
+    }
 }
